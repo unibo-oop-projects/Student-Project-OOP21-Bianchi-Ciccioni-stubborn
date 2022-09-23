eb4452f41ba82407acab4a0fc147d51c4fac4a79
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2022-09-23 21:53:44 +0200

    Add files via upload
    Implementati eventi relativi ad indicatore vita (riduzione ed aumento) e punteggio (aumento).

---
+++ gameBackgroundsControl.java
@@ -0,0 +1,36 @@
+
+public class gameBackgroundsControl {
+    private void refillLife() {
+	lifeLabel.setText("");
+	for (Integer i = 0; i < lifePoints; i++) {
+		lifeLabel.addText("♥");
+	}
+    }
+    new EventHandler() {
+    	public void hurtLife(ActionEvent event) {
+    		lifePoints--;
+    		if (lifePoints == 0) {
+    			gameOver();
+		} end if;
+    		lifeLabel.setText("!!GOT HIT!!");
+		wait(Duration.seconds(1));
+		refillLife();
+    	}
+    }
+    new EventHandler() {
+    	public void healLife(ActionEvent event) {
+    		if (lifePoints < MAXLIVES) {
+        		lifePoints++;
+	    		lifeLabel.setText("!!LIFE GAINED!!");
+			wait(Duration.seconds(1));
+			refillLife();
+		} end if;
+    	}
+    }
+    new EventHandler() {
+    	public void updateScore(ActionEvent event, Integer gain) {
+    		scorePoints += gain;
+    		scoreLabel.setText(scorePoints.toString());
+    	}
+    }
+}
