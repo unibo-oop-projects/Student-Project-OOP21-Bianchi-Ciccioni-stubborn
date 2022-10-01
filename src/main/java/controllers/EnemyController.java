5009e61782ea67c559a12b9df3931d2d7d3ba97d
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2022-10-01 12:34:09 +0200

    merge settings and project files, added EnemyController and a new layout

---
+++ src/main/java/controllers/EnemyController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import com.google.common.collect.ImmutableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.util.stream.Collectors;
+
+public final class EnemyController {
+    
+
+}
