95cdb9b184bad984979ae90786957134aab59f3a
AlessandroCacciaguerra <113693211+AlessandroCacciaguerra@users.noreply.github.com>
2022-10-05 08:08:52 +0200

    Add files via upload

---
+++ src/main/java/controllers/menuUIController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public static final class menuUIController {
+    @FXML
+    private static final Button btnPlay;
+    
+    @FXML
+    private static final Button btnScore;
+
+    @FXML
+    public static void btnPlayOnClickHandler() {
+        application.Menu.hideMenu();
+        application.Backgrounds.main();
+        controllers.backgroundsUIController.startTimer();
+        controllers.backgroundsUIController.refillLife();
+    }
+
+    @FXML
+    public static void btnScoreOnClickHandler() {
+        application.Menu.hideMenu();
+        /* startBoard();	sostituire a startBoard il nome dato alla
+         *  funzione che apre la visualizzazione del set dei punteggi */
+    }
+}
