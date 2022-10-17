f8689e00647b2690e33f1ab35644525db9672204
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2022-10-17 11:49:16 +0200

    renamed EnemyController to BoardController, added skeletal structure

---
+++ src/main/java/controllers/BoardController.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import com.google.common.collect.ImmutableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import models.RandomSpawnStrategy;
+import models.SpawnStrategy;
+import models.WorldMap;
+import javafx.scene.input.InputEvent;
+
+import java.util.stream.Collectors;
+
+public final class BoardController {
+    
+    static private final int WIDTH = 51;
+    static private final int HEIGHT = 51;
+    static private final int ENEMIES = 8;
+    static private final int COLLECTABLES = 10;
+    
+    private WorldMap gameWorldMap;
+    private SpawnStrategy spawnStrat = new RandomSpawnStrategy();
+    
+    
+    @FXML
+    public void initialize() {
+        this.gameWorldMap = new WorldMap(WIDTH, HEIGHT, ENEMIES, COLLECTABLES, spawnStrat);
+    }
+    
+
+}
