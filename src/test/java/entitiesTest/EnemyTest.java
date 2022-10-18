6da07d261b55bff7b30d25b15680f25907499071
Mario Ciccioni <mario.ciccioni@studio.unibo.it>
2022-10-18 21:10:36 +0200

    Testing of Enemy behavior implemented correctly

--- src/test/java/entitiesTest/EnemyTest.java
+++ src/test/java/entitiesTest/EnemyTest.java
@@ -3,39 +3,29 @@ package entitiesTest;
 import org.junit.jupiter.api.Test;
 
 import models.AiEnemy;
-import models.Collectable;
-import models.CollectableImpl;
 import models.Enemy;
 import models.EnemyImpl;
 import models.Entity;
 import models.FocusAiEnemy;
-import models.Player;
-import models.PlayerImpl;
 import models.Point2D;
 import models.RandomAiEnemy;
 import models.RandomSpawnStrategy;
 import models.SpawnStrategy;
 import models.WorldMap;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EnemyTest {
     
     private Point2D startPlayerPosition = new Point2D(1,1);
-    private int PlayerHealth = 3;
-    private Player p = new PlayerImpl(startPlayerPosition, PlayerHealth);
     
     private Point2D startEnemy1Position = new Point2D(5,5);
     private int Enemy1Health = 1;
     
-    private Point2D startEnemy2Position = new Point2D(5,5);
+    private Point2D startEnemy2Position = new Point2D(10,10);
     private int Enemy2Health = 1;
     
     private AiEnemy ai1 = new RandomAiEnemy();
@@ -57,18 +47,22 @@ public class EnemyTest {
         Map<Point2D,Optional<Entity>> board = worldMap.getBoard();
         Point2D newEnemyPos = e1.getAi().move(board, startPlayerPosition, startEnemy1Position);
         e1.setPosition(newEnemyPos);
-        assertEquals(e1.getAi(), ai1);
+        System.out.println(newEnemyPos);
+        //assertEquals(e1.getAi(), ai1);
         assertNotEquals(e1.getPosition(), startEnemy1Position);
     }
     
     @Test
     public void testFocusAiEnemy() {
-        //int distance = ((startEnemy2Position.getX() – startPlayerPosition.getX()) + (startEnemy2Position.getY() – startPlayerPosition.getY()));
+        int dBefore = ((e2.getPosition().getX() - startPlayerPosition.getX()) + (e2.getPosition().getY() - startPlayerPosition.getY()));
+        System.out.println("before: " + dBefore);
         Map<Point2D,Optional<Entity>> board = worldMap.getBoard();
         Point2D newEnemyPos = e2.getAi().move(board, startPlayerPosition, startEnemy2Position);
         e2.setPosition(newEnemyPos);
-        assertEquals(e1.getAi(), ai1);
-        assertEquals(e1.getPosition(), startEnemy2Position);
+        int dAfter = ((e2.getPosition().getX() - startPlayerPosition.getX()) + (e2.getPosition().getY() - startPlayerPosition.getY()));
+        System.out.println("after: " + dAfter);
+        //assertEquals(e2.getAi(), ai2);
+        assertTrue(dBefore > dAfter);
     }
 
 }
