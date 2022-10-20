71d91c1ad38b8dd33813cdbd73d9352cc9879f0c
Mario Ciccioni <mario.ciccioni@studio.unibo.it>
2022-10-20 12:17:03 +0200

    test testMovePlayer added in PlayerTest class

--- src/test/java/entitiesTest/PlayerTest.java
+++ src/test/java/entitiesTest/PlayerTest.java
@@ -2,27 +2,41 @@ package entitiesTest;
 
 import org.junit.jupiter.api.Test;
 
-import models.Collectable;
-import models.CollectableImpl;
+import models.Entity;
+import models.MOVEMENT;
 import models.Player;
-import models.PlayerImpl;
 import models.Point2D;
+import models.RandomSpawnStrategy;
+import models.SpawnStrategy;
+import models.WorldMap;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
     
-    private Point2D startPosition = new Point2D(5,5);
-    private int health = 3;
-    private Player p = new PlayerImpl(startPosition, health);
+    static int WIDTH = 51;
+    static int HEIGHT = 51;
+    static int NUM_ENEMIES = 5;
+    static int NUM_COLLECTABLES = 15;
+    static int EXPECTED_SIZE = 5;
+    
+    private SpawnStrategy randomStrategy = new RandomSpawnStrategy();
+    private WorldMap worldMap = new WorldMap(WIDTH,HEIGHT,NUM_ENEMIES,NUM_COLLECTABLES, randomStrategy);
+    Point2D startPlayerPos = new Point2D(WIDTH/2,HEIGHT/2);
     
     @Test
     public void testMovePlayer() {
-        // TODO test for check if player can move inside the map
+        worldMap.movePlayer(MOVEMENT.LEFT);
+        worldMap.movePlayer(MOVEMENT.UP);
+        Map<Point2D,Optional<Entity>> board = worldMap.getBoard();
+        Point2D leftPosition = new Point2D(24,26);
+        assertTrue(board.get(leftPosition).isPresent());
+        assertTrue(board.get(leftPosition).get() instanceof Player);
+        assertTrue(board.get(startPlayerPos).isEmpty());
+        
     }
 
 }
