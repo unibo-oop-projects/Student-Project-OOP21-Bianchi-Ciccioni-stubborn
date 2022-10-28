01b27fb869cbde45f6adea0c55af95702cd9db9d
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2022-10-28 13:35:37 +0200

    same adjustments made for EnemyTest and PlayerTest

--- src/test/java/entitiesTest/EnemyTest.java
+++ src/test/java/entitiesTest/EnemyTest.java
@@ -11,6 +11,7 @@ import models.Point2D;
 import models.RandomAiEnemy;
 import models.RandomSpawnStrategy;
 import models.SpawnStrategy;
+import models.WorldMap;
 import models.WorldMapImpl;
 
 import java.util.Map;
@@ -40,7 +41,7 @@ public class EnemyTest {
     static int NUM_COLLECTABLES = 15;
     
     private SpawnStrategy randomStrategy = new RandomSpawnStrategy();
-    private WorldMapImpl worldMap = new WorldMapImpl(WIDTH,HEIGHT,NUM_ENEMIES,NUM_COLLECTABLES, randomStrategy);
+    private WorldMap worldMap = new WorldMapImpl(WIDTH,HEIGHT,NUM_ENEMIES,NUM_COLLECTABLES, randomStrategy);
     
     @Test
     public void testRandomAiEnemy() {
--- src/test/java/entitiesTest/PlayerTest.java
+++ src/test/java/entitiesTest/PlayerTest.java
@@ -8,6 +8,7 @@ import models.Player;
 import models.Point2D;
 import models.RandomSpawnStrategy;
 import models.SpawnStrategy;
+import models.WorldMap;
 import models.WorldMapImpl;
 
 import java.util.Map;
@@ -24,7 +25,7 @@ public class PlayerTest {
     static int EXPECTED_SIZE = 5;
     
     private SpawnStrategy randomStrategy = new RandomSpawnStrategy();
-    private WorldMapImpl worldMap = new WorldMapImpl(WIDTH,HEIGHT,NUM_ENEMIES,NUM_COLLECTABLES, randomStrategy);
+    private WorldMap worldMap = new WorldMapImpl(WIDTH,HEIGHT,NUM_ENEMIES,NUM_COLLECTABLES, randomStrategy);
     Point2D startPlayerPos = new Point2D(WIDTH/2,HEIGHT/2);
     
     @Test
